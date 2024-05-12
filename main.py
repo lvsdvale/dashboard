4fa90eba7e112cf5ae8138216ba2cdbf0db51913
Eliaquim Souza <eliaquim@alunos.utfpr.edu.br>
2024-05-12 18:48:51 -0300

    Atualizacao

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import json
 import time
 import threading
 
-class Tarefa:
+class TarefaProcesso:
     def __init__(self, pid, ppid, memoria, cpu, leitura, escrita):
         self.pid = pid
         self.ppid = ppid
@@ -13,26 +13,60 @@ class Tarefa:
         self.leitura = leitura
         self.escrita = escrita
 
-    def __str__(self):
-        return f"{self.pid:<10} {self.ppid:<10} {self.memoria:<10} {self.cpu:<10} {self.leitura:<10} {self.escrita:<10}"
+
+class TarefaMemoria:
+    def __init__(self, pid, total_memory, code_memory, heap_memory, stack_memory, total_pages, code_pages, heap_pages, stack_pages):
+        self.pid = pid
+        self.total_memory = total_memory
+        self.code_memory = code_memory
+        self.heap_memory = heap_memory
+        self.stack_memory = stack_memory
+        self.total_pages = total_pages
+        self.code_pages = code_pages
+        self.heap_pages = heap_pages
+        self.stack_pages = stack_pages
+
+class DadosGlobais:
+    def __init__(self, pid, ppid, memoria, cpu, leitura, escrita):
+        self.pid = pid
+        self.ppid = ppid
+        self.memoria = memoria
+        self.cpu = cpu
+        self.leitura = leitura
+        self.escrita = escrita
 
 class GerenciadorTarefas:
     def __init__(self):
         self.window = tk.Tk()
         self.window.title("Gerenciador de Tarefas")
 
-        # Criar tabela de tarefas
-        self.tabela = ttk.Treeview(self.window, columns=("PID", "PPID", "Memória", "CPU", "Leitura", "Escrita"))
-        self.tabela.heading("PID", text="PID")
-        self.tabela.heading("PPID", text="PPID")
-        self.tabela.heading("Memória", text="Memória")
-        self.tabela.heading("CPU", text="CPU")
-        self.tabela.heading("Leitura", text="Leitura")
-        self.tabela.heading("Escrita", text="Escrita")
-        self.tabela.pack(expand=True, fill=tk.BOTH, side="top")
+        # Criar tabela_processos de tarefas
+        self.tabela_processos = ttk.Treeview(self.window, columns=("PID", "PPID", "Memória", "CPU", "Leitura", "Escrita"))
+        self.tabela_processos.heading("PID", text="PID")
+        self.tabela_processos.heading("PPID", text="PPID")
+        self.tabela_processos.heading("Memória", text="Memória")
+        self.tabela_processos.heading("CPU", text="CPU")
+        self.tabela_processos.heading("Leitura", text="Leitura")
+        self.tabela_processos.heading("Escrita", text="Escrita")
+        self.tabela_processos.pack(expand=True, fill=tk.BOTH, side="top")
+        
+        self.tabela_memoria = ttk.Treeview(self.window, columns=("PID", "Memoria Total", "Memoria Codigo", "Memoria Heap", "Memoria Stack",
+                                                                 "Total de Paginas", "Paginas Codigos", "Paginas Heap", "Paginas Stack"))
+        self.tabela_memoria.heading("PID", text="PID")
+        self.tabela_memoria.heading("Memoria Total", text="Memoria Total")
+        self.tabela_memoria.heading("Memoria Codigo", text="code_memory")
+        self.tabela_memoria.heading("Memoria Heap", text="heap_memory")
+        self.tabela_memoria.heading("Memoria Stack", text="stack_memory")
+        self.tabela_memoria.heading("Total de Paginas", text="total_pages")
+        self.tabela_memoria.heading("Paginas Codigos", text="code_pages")
+        self.tabela_memoria.heading("Paginas Heap", text="heap_pages")
+        self.tabela_memoria.heading("Paginas Stack", text="stack_pages")
+        self.tabela_memoria.pack(expand=True, fill=tk.BOTH, side="top")
+        
 
         # Carregar tarefas iniciais
-        self.carregar_tarefas()
+        self.carregar_tarefas_processos()
+        self.carregar_tarefas_memoria()
 
         # Iniciar thread de atualização automática
         self.atualizar_thread = threading.Thread(target=self.atualizar_tarefas_automaticamente)
@@ -41,54 +75,73 @@ class GerenciadorTarefas:
         # Executar loop principal da interface
         self.window.mainloop()
 
-    def limpar_tabela(self):
-        self.tabela.delete(*self.tabela.get_children())
+    def limpar_tabela_processos(self):
+        self.tabela_processos.delete(*self.tabela_processos.get_children())
+    
+    def limpar_tabela_memoria(self):
+        self.tabela_processos.delete(*self.tabela_memoria.get_children())
 
-    def carregar_tarefas(self):
-        with open("tarefas.json", "r") as arquivo:
+    def carregar_tarefas_processos(self):
+        with open("api\processes.json", "r") as arquivo:
             dados = json.load(arquivo)
 
-        tarefas = []
+        tarefas_processos = []
         for tarefa in dados:
             pid = tarefa["pid"]
             ppid = tarefa["ppid"]
-            memoria = tarefa["memoria"]
-            cpu = tarefa["cpu"]
-            leitura = tarefa["leitura"]
-            escrita = tarefa["escrita"]
+            memoria = str(tarefa["mem_usage_mb"]) + " Mb"
+            cpu = str(tarefa["cpu_usage"]) + "%"
+            leitura = str(tarefa["total_read_bytes"]) + " b"
+            escrita = str(tarefa["total_write_bytes"]) + " b"
 
-            nova_tarefa = Tarefa(pid, ppid, memoria, cpu, leitura, escrita)
-            tarefas.append(nova_tarefa)
+            nova_tarefa = TarefaProcesso(pid, ppid, memoria, cpu, leitura, escrita)
+            tarefas_processos.append(nova_tarefa)
+        
+        self.limpar_tabela_processos()
+        for tarefa in tarefas_processos:
+                self.inserir_tarefa_processo(tarefa)
     
-    
-        self.limpar_tabela()
-        for tarefa in tarefas:
-            self.inserir_tarefa(tarefa)
+    def carregar_tarefas_memoria(self):
+        with open("api\processes_memory.json", "r") as arquivo:
+            dados = json.load(arquivo)
+        tarefas_memoria = []
+        for tarefa in dados:
+            pid = tarefa["pid"]
+            total_memory = tarefa["total_memory"]
+            code_memory = tarefa["code_memory"]
+            heap_memory = tarefa["heap_memory"]
+            stack_memory = tarefa["stack_memory"]
+            total_pages = tarefa["total_pages"]
+            code_pages = tarefa["code_pages"]
+            heap_pages = tarefa["heap_pages"]
+            stack_pages = tarefa["stack_pages"]
+            nova_tarefa = TarefaMemoria(pid, total_memory, code_memory, heap_memory, stack_memory, total_pages, code_pages, heap_pages, stack_pages)
+            tarefas_memoria.append(nova_tarefa)
 
-    def inserir_tarefa(self, tarefa):
+
+        self.limpar_tabela_memoria()
+        for tarefa in tarefas_memoria:
+            self.inserir_tarefa_memoria(tarefa)
+
+    def inserir_tarefa_processo(self, tarefa):
         valores = (tarefa.pid, tarefa.ppid, tarefa.memoria, tarefa.cpu, tarefa.leitura, tarefa.escrita)
-        self.tabela.insert("", tk.END, values=valores)
+        self.tabela_processos.insert("", tk.END, values=valores)
+    
+    def inserir_tarefa_memoria(self, tarefa):
+        valores = (tarefa.pid, tarefa.total_memory, tarefa.code_memory, tarefa.heap_memory, tarefa.total_pages, tarefa.code_pages, tarefa.heap_pages, tarefa.stack_pages)
+        self.tabela_memoria.insert("", tk.END, values=valores)
 
-    def filtrar_tarefas(self, event):
-        termo_filtro = self.filtro_entry.get()
-        tarefas_filtradas = []
-
-        for tarefa in self.tarefas:
-            if termo_filtro.lower() in str(tarefa).lower():
-                tarefas_filtradas.append(tarefa)
-
-        self.limpar_tabela()
-        for tarefa in tarefas_filtradas:
-            self.inserir_tarefa(tarefa)
-
-    def atualizar_tarefas(self):
-        self.carregar_tarefas()
+    def atualizar_tarefas_processos(self):
+        self.carregar_tarefas_processos()
+        
+    def atualizar_tarefas_memoria(self):
+        self.carregar_tarefas_memoria()  
 
     def atualizar_tarefas_automaticamente(self):
         while True:
-            self.atualizar_tarefas()
+            self.atualizar_tarefas_processos()
+            self.atualizar_tarefas_memoria()
             time.sleep(6)
 
 if __name__ == "__main__":
     app = GerenciadorTarefas()
-    app.executar()
